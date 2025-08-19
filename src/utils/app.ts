a2f30406e77bdbe10786e9f563d3b87343a3dc43
kassadin <369642339@qq.com>
2025-08-19 09:38:18 +0800

    feat: subcode

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,6 +1,32 @@
 import { isAndroid, isIOS } from './constants'
 const __w = window as any
 
+// 统一回调原生
+export function callNative(options = {}, callback?: (data: any) => void) {
+  const { handler, data = {} } = options as any
+  let cb = ''
+  if (callback) {
+    cb = `__callback_${Date.now()}_${new Array(6)
+      .fill('')
+      .map(() => Math.floor(Math.random() * 10))
+      .join('')}`
+  }
+  if (__w.webkit && __w.webkit.messageHandlers && __w.webkit.messageHandlers.jsBridge) {
+    __w.webkit.messageHandlers.jsBridge.postMessage({ handler, cb, data })
+  } else if (__w.app && __w.app[handler]) {
+    __w.app[handler](JSON.stringify({ data, cb }))
+  }
+  if (cb) {
+    __w[cb] = (data: any) => {
+      try {
+        data = JSON.parse(data)
+      } catch {}
+      callback?.(data)
+      delete __w[cb]
+    }
+  }
+}
+
 // 返回APP
 export const backToApp = (path = '') => {
   try {
@@ -22,8 +48,6 @@ export const backToApp = (path = '') => {
 // 获取用户信息和公共参数
 export const getUserInfo = () => {
   console.log('getUserInfo => ', __w)
-  console.log('isIOS', isIOS)
-  console.log('isAndroid', isAndroid)
   try {
     if (isIOS) {
       return __w.webkit.messageHandlers.getUserInfo.postMessage('')
